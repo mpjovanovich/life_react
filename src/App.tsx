3224d9bb2f6552a8a707b2086ed08a42045abf9b
Mike Jovanovich <mpjovanovich@gmail.com>
2023-06-07 20:03:25 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+    return (
+<>
+    <div id="logo_container">
+        <h1 id="logo_text">Game of Life</h1>
+    </div>
+    <p>Stuff and things.</p>
+</>
+    )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  // Had to add this bit to get HMR to work.
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
+})
