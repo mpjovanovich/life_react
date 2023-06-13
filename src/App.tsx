d7d8257766cb79d3be9f4f566d57418216f09bb9
Mike Jovanovich <mpjovanovich@gmail.com>
2023-06-13 16:08:31 -0400

    More better styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,23 @@ import Life from './Life'
 // is small.
 // * Add favicon. favicon should match cell icons.
 // * Allow user to select grid size. Maybe use a vertical and horizontal slider. Should go below the game controls.
-// * Allow user to select time increment. Should go below the game controls.
+// * Allow user to select time increment using a ??? control. Should go below the game controls.
+// * Disable the new elements above.
 // * Add a link to the github repo in the footer.
 // * Publish/host on replit. Send to friends and family.
 
 // React TypeScript Cheatsheets:
 // https://github.com/typescript-cheatsheets/react#reacttypescript-cheatsheets
 
-const NUM_ROWS:number = 15;
-const NUM_COLS:number = 15;
-const STEP_TIME_MS:number = 500;
-
 const App:React.FC = () => {
+    // State for parameters.
+    const [stepIncrement,setStepIncrement] = React.useState(500);
+    const [columnCount,setColumnCount] = React.useState(15);
+    const [rowCount,setRowCount] = React.useState(15);
+
     // The useMemo hook makes sure that the Life object is only created once; not
     // every time the component re-renders.
-    const life = React.useMemo(() => new Life(NUM_COLS, NUM_ROWS), []);
+    const life = React.useMemo(() => new Life(columnCount, rowCount), [columnCount,rowCount]);
 
     // This is the hoisted variable for any component that needs to know about the game state.
     const [_,setGameState] = React.useState(life.board);
@@ -46,7 +48,7 @@ const App:React.FC = () => {
             setTimerId (setInterval(() => {
                 life.step(); 
                 setGameState(life.board)
-            }, STEP_TIME_MS));
+            }, stepIncrement));
             setTimerText("STOP");
         }
     }
@@ -61,21 +63,44 @@ const App:React.FC = () => {
 
     {/* Maybe make this a component later. */}
     <div id="game-container">
-        <GameGrid 
-            numCols={life.numCols}
-            numRows={life.numRows}
-            isCellAlive={life.isCellAlive} 
-            onToggleCell={handleCellToggle} 
-        />
+        <div id="game-cells">
+            <GameGrid 
+                numCols={life.numCols}
+                numRows={life.numRows}
+                isCellAlive={life.isCellAlive} 
+                onToggleCell={handleCellToggle} 
+            />
+        </div>
+
+        <div id="game-parameters">
+            <div className='game-parameter-container clear'>
+                <label htmlFor="rows">Rows:</label>
+                <input disabled={timerId != null} type="range" id="rows" name="rows" min="10" max="50" value={rowCount}
+                    onChange={(e) => setRowCount(parseInt(e.target.value))}></input>
+            </div>
+            <div className='game-parameter-container clear'>
+                <label htmlFor="cols">Columns:</label>
+                <input disabled={timerId != null} type="range" id="cols" name="cols" min="10" max="50" value={columnCount}
+                    onChange={(e) => setColumnCount(parseInt(e.target.value))}></input>
+            </div>
+            <div className='game-parameter-container clear'>
+                <label htmlFor="increment">Step Increment (ms):</label>
+                <input disabled={timerId != null} type="number" id="quantity" name="quantity" min="100" max="" step={100} value={stepIncrement} 
+                    onChange={(e) => setStepIncrement(parseInt(e.target.value))}></input>
+            </div>
+        </div>
+        <div className='clear' />
 
         <div id="game-controls">
             <div id="generation">
                 <span className="float-left">Generation: </span>
                 <span className="float-right" style={{fontWeight: `${timerId ? "600" : "normal"}`}}>{life.generation}</span>
             </div>
-            <button id="timer-button" className={`${timerId && "running-button"}`} onClick={() => {handleTimerToggle();}}>{timerText}</button>
-            <button disabled={timerId != null} id="step-button" onClick={() => {life.step(); setGameState(life.board);}}>STEP</button>
-            <button disabled={timerId != null} id="reset-button" onClick={() => { life.resetBoard(); setGameState(life.board);}}>CLEAR</button>
+            <div id="game-button-container">
+                <button id="timer-button" className={`${timerId && "running-button"}`} onClick={() => {handleTimerToggle();}}>{timerText}</button>
+                <button disabled={timerId != null} id="step-button" onClick={() => {life.step(); setGameState(life.board);}}>STEP</button>
+                <button disabled={timerId != null} id="reset-button" onClick={() => { life.resetBoard(); setGameState(life.board);}}>CLEAR</button>
+            </div>
         </div>
     </div>
 </>
